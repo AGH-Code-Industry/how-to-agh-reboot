7756bd7753c657adf59b52b90775580c4eeee78f
Xenoneqq <realfatboimlg@gmail.com>
2025-02-22 18:52:22 +0100

    Added simple reward page

--- app/prizes/page.tsx
+++ app/prizes/page.tsx
@@ -1,27 +1,30 @@
-import Reward from "@/components/reward/Reward";
+import { PageLayout, PageTitle } from '@/components/layout/PageLayout';
+import Reward from '@/components/reward/Reward';
 
 export default function Prizes() {
-
   const tempNagroda = {
     reward: 'Kubek z Uśmieszkiem',
     requirement: 'Wykonaj 5 zadań rangi średniej lub trudnej.',
     completion: 0.33,
-  }
+  };
 
   const tempNagrodaFull = {
     reward: 'Kubek z Uśmieszkiem',
     requirement: 'Wykonaj 5 zadań rangi średniej lub trudnej.',
     completion: 1,
-  }
+  };
 
   return (
-    <>
-      <div>Nagrody</div>
-      <div style={{ display: 'flex', flexDirection: 'column', gap: '20px' }}>
-        <div><Reward {...tempNagroda}></Reward></div>
-        <div><Reward {...tempNagroda}></Reward></div>
-        <div><Reward {...tempNagrodaFull}></Reward></div>
+    <PageLayout>
+      <PageTitle>Nagrody</PageTitle>
+      <div className="flex flex-col gap-4">
+        <Reward {...tempNagroda} />
+        <Reward {...tempNagroda} />
+        <Reward {...tempNagrodaFull} />
+        <Reward {...tempNagrodaFull} />
+        <Reward {...tempNagroda} />
+        <Reward {...tempNagroda} />
       </div>
-    </>
-  )
+    </PageLayout>
+  );
 }
