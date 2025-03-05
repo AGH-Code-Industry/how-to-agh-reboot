02a18359885fe58f3d49ef8ef7939f5966c50e24
MBrosik <michal.bros1@gmail.com>
2025-03-05 22:11:15 +0100

    Clear comments

--- components/map/AGHEvents.tsx
+++ components/map/AGHEvents.tsx
@@ -1,7 +1,6 @@
 import { AGHEvent } from '@/types/Map/AGHEvent';
 import { Marker } from 'react-map-gl/maplibre';
 import { LngLat, Offset, Popup as Popup2 } from 'maplibre-gl';
-// import { Popup } from 'react-map-gl/maplibre';
 import 'maplibre-gl/dist/maplibre-gl.css';
 
 import './AGHEvents.css';
@@ -50,13 +49,11 @@ export default function AGHEvents(props: Props) {
         const popup = new Popup2({
           offset: popupOffsets,
           className: 'text-black',
-          // closeButton: false,
         })
           .setLngLat(new LngLat(event.lng, event.ltd))
           .setHTML(getHTML(event))
           .setMaxWidth('300px');
 
-        // return <MarkerComponent event={event} key={event.name} />;
         return (
           <Marker
             key={event.name}
@@ -70,46 +67,3 @@ export default function AGHEvents(props: Props) {
     </>
   );
 }
-
-// const MarkerComponent = ({ event }: { event: AGHEvent }) => {
-//   const [open, setOpen] = useState(false);
-//
-//   return (
-//     <>
-//       <Marker
-//         longitude={event.lng}
-//         latitude={event.ltd}
-//         color="red"
-//         onClick={() => {
-//           setOpen(!open);
-//         }}
-//       />
-//       <Popup
-//         longitude={event.lng}
-//         latitude={event.ltd}
-//         className={'text-black'}
-//         offset={popupOffsets}
-//         maxWidth={'300px'}
-//         onClose={() => setOpen(false)}
-//         onOpen={() => setOpen(true)}
-//       >
-//         <div>
-//           <h1 className="text-center text-lg">{event.name}</h1>
-//           <p className="mt-3">
-//             <b>Czas:</b> {event.startTime} - {event.endTime}
-//           </p>
-//           <p>
-//             <b>Typ:</b> {event.type}
-//           </p>
-//           <p>
-//             <b>Temat:</b> {event.topic}
-//           </p>
-//           <p>
-//             <b>Wydział:</b> {event.faculty}
-//           </p>
-//           <p className="mt-3">{event.description}</p>
-//         </div>
-//       </Popup>
-//     </>
-//   );
-// };
