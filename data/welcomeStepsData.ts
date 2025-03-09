5bb0a5b2a8b8946c1c915dc66c834eb62fdeee6c
Michał Ślęzak <michaldakol@gmail.com>
2025-03-09 18:44:13 +0100

    Feat: Update welcome steps; configure image handling

--- data/welcomeStepsData.ts
+++ data/welcomeStepsData.ts
@@ -27,12 +27,54 @@ export const welcomeSteps: WelcomeStep[] = [
   },
   {
     id: 2,
-    title: 'Personalizacja',
-    description: 'Dostosuj ustawienia konta do swoich potrzeb.',
+    title: 'Mapa',
+    description:
+      'Zwiedzaj kampus AGH i odkrywaj nowe miejsca. Dołączaj do tematycznych tras i pogłębiaj swoją wiedzę.',
+    image: {
+      id: 2,
+      src: `https://placehold.co/100x240`,
+      width: 100,
+      height: 240,
+    },
   },
   {
     id: 3,
-    title: 'Gotowe do działania!',
-    description: 'Możesz teraz zacząć korzystać z aplikacji.',
+    title: 'Zadania',
+    description:
+      'Wykonuj zadania i odblokowuj Nagrody. Zadania są dostępne na mapie. Zadanie zostaje zaliczone po zeskanowaniu odpowiedniego kodu QR.',
+    image: {
+      id: 3,
+      src: `https://placehold.co/100x240`,
+      width: 100,
+      height: 240,
+    },
+  },
+  {
+    id: 4,
+    title: 'Skaner',
+    description: 'Skanuj kody QR, aby zaliczyć zadania i zdobywać punkty.',
+    image: {
+      id: 4,
+      src: `https://placehold.co/100x240`,
+      width: 100,
+      height: 240,
+    },
+  },
+  {
+    id: 5,
+    title: 'Nagrody',
+    description:
+      'Po zaliczeniu wymaganych zadań odbieraj nagrody. Nagrody do odebrania będą w namiocie.',
+    image: {
+      id: 5,
+      src: `https://placehold.co/100x240`,
+      width: 100,
+      height: 240,
+    },
+  },
+  {
+    id: 6,
+    title: 'Zaczynajmy',
+    description: 'To już wszystko. Zaczynajmy zwiedzanie!',
   },
 ];
