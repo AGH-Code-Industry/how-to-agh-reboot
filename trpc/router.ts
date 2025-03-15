4cfe73dcf48e5ba269436138678b31d792a9c62f
Spookyless <spookyless.info@gmail.com>
2025-03-15 21:53:08 +0100

    Create sample get routes

--- trpc/router.ts
+++ trpc/router.ts
@@ -1,8 +1,12 @@
 import { router } from './init';
+import { eventsRouter } from './routers/events';
 import { exampleRouter } from './routers/example';
+import { quizesRouter } from './routers/quizes';
 
 export const appRouter = router({
   example: exampleRouter,
+  events: eventsRouter,
+  quizes: quizesRouter,
 });
 
 export type AppRouter = typeof appRouter;
