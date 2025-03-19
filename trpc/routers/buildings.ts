8436774165f94a7c4a3d8d50745bd2676bd2ad1f
Agata Marczyk <115737563+soberyoda@users.noreply.github.com>
2025-03-19 19:44:18 +0100

    Update buildings.ts

--- trpc/routers/buildings.ts
+++ trpc/routers/buildings.ts
@@ -1,53 +1,36 @@
 import { procedure, router } from '../init';
 import { prisma } from '@/prisma/prisma';
 import { z } from 'zod';
-import { buildingDOtoDTO, buildingEntryDOtoDTO } from '@/types/Building';
+import { buildingDOtoDTO } from '@/types/Building';
 
 export const buildingsRouter = router({
-  getBuildings: procedure.query(async () => {
-    const buildings = await prisma.building.findMany({
-      include: {
-        building_entries: true,
-      },
-    });
-    return buildings.map(buildingDOtoDTO);
-  }),
-
-  getBuildingById: procedure
-    .input(
-      z.object({
-        buildingId: z.number().positive(),
-      })
-    )
-    .query(async ({ input }) => {
-      const building = await prisma.building.findUnique({
-        where: {
-          building_id: input.buildingId,
-        },
-        include: {
-          building_entries: true,
-        },
-      });
-      if (!building) {
-        throw new Error('Building not found');
-      }
-      return buildingDOtoDTO(building);
-    }),
-
-  getBuildingEntries: procedure
+  getBuildings: procedure
     .input(
       z.object({
         buildingId: z.number().positive().optional(),
       })
     )
     .query(async ({ input }) => {
-      const whereClause = input.buildingId ? { building_id: input.buildingId } : {};
-      const entries = await prisma.buildingEntry.findMany({
-        where: whereClause,
-        include: {
-          building: true,
-        },
-      });
-      return entries.map(buildingEntryDOtoDTO);
+      if (input.buildingId) {
+        const building = await prisma.building.findUnique({
+          where: {
+            building_id: input.buildingId,
+          },
+          include: {
+            building_entries: true,
+          },
+        });
+        if (!building) {
+          throw new Error('Building not found');
+        }
+        return buildingDOtoDTO(building);
+      } else {
+        const buildings = await prisma.building.findMany({
+          include: {
+            building_entries: true,
+          },
+        });
+        return buildings.map(buildingDOtoDTO);
+      }
     }),
 });
