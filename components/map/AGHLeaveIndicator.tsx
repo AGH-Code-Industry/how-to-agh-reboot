16a154cbcc40ba542ebd4445c02185f91f041249
Michal Karpierz <michalk101010@gmail.com>
2025-04-07 19:48:36 +0200

    Fixed typo

--- components/map/AGHLeaveIndicator.tsx
+++ components/map/AGHLeaveIndicator.tsx
@@ -4,7 +4,7 @@ function AGHLeaveIndicator() {
   return (
     <div className="pointer-events-none absolute left-0 top-0 box-border size-full border-4 border-red-600">
       <div className="absolute bottom-3 left-1/2 -translate-x-1/2 rounded-md bg-red-600 p-3">
-        Znajdujesz się poza poza AGH
+        Znajdujesz się poza AGH
       </div>
     </div>
   );
