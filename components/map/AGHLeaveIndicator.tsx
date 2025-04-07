9e683d0e22906e8bbaaa386ccfbcde387a0ed018
Michal Karpierz <michalk101010@gmail.com>
2025-04-07 21:40:37 +0200

    Changed red to errorAlert color

--- components/map/AGHLeaveIndicator.tsx
+++ components/map/AGHLeaveIndicator.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 function AGHLeaveIndicator() {
   return (
-    <div className="pointer-events-none absolute left-0 top-0 box-border size-full border-4 border-red-600">
-      <div className="absolute bottom-3 left-1/2 -translate-x-1/2 rounded-md bg-red-600 p-3">
+    <div className="pointer-events-none absolute left-0 top-0 box-border size-full border-4 border-errorAlert-foreground">
+      <div className="absolute bottom-3 left-1/2 -translate-x-1/2 rounded-md bg-errorAlert-foreground p-3">
         Znajdujesz się poza AGH
       </div>
     </div>
