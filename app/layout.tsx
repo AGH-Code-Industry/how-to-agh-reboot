a165b29449bd11bbd3a4e75478767b653300d56d
Spookyless <spookyless.info@gmail.com>
2025-04-11 00:53:57 +0200

    Fix: Wrong mode for layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({
         >
           <TRPCProvider>
             <AuthProvider>
-              <WelcomeOverlay mode={'force-open'} />
+              <WelcomeOverlay mode={'automatic'} />
               <main className="grow overflow-y-auto">{children}</main>
               <NavBar />
               <Toaster position="top-center" />
