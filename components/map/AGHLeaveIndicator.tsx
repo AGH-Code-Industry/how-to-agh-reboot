b537c89b9ea450aea24e177da0b4448250478da9
Michal Karpierz <michalk101010@gmail.com>
2025-04-10 18:01:11 +0200

    Improved AGHLeaveIndicator style

--- components/map/AGHLeaveIndicator.tsx
+++ components/map/AGHLeaveIndicator.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function AGHLeaveIndicator() {
   return (
     <div className="pointer-events-none absolute left-0 top-0 box-border size-full border-4 border-errorAlert-foreground">
-      <div className="absolute bottom-3 left-1/2 -translate-x-1/2 whitespace-nowrap rounded-md bg-errorAlert-foreground p-3">
+      <div className="absolute bottom-3 left-1/2 w-max max-w-[80%] -translate-x-1/2 rounded-md bg-errorAlert-foreground p-3 text-center">
         Znajdujesz się poza obszarem dni otwartych AGH
       </div>
     </div>
