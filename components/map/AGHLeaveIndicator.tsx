753aa1beff246321308e05bd2bb309c7a77a413a
Michal Karpierz <michalk101010@gmail.com>
2025-04-10 17:38:48 +0200

    Shrank highlighted area on the map

--- components/map/AGHLeaveIndicator.tsx
+++ components/map/AGHLeaveIndicator.tsx
@@ -4,7 +4,7 @@ function AGHLeaveIndicator() {
   return (
     <div className="pointer-events-none absolute left-0 top-0 box-border size-full border-4 border-errorAlert-foreground">
       <div className="absolute bottom-3 left-1/2 -translate-x-1/2 whitespace-nowrap rounded-md bg-errorAlert-foreground p-3">
-        Znajdujesz się poza AGH
+        Znajdujesz się poza obszarem dni otwartych AGH
       </div>
     </div>
   );
