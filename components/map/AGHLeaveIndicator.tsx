c4316198ce271b8a840db76345abc12dd86cdac4
Michal Karpierz <michalk101010@gmail.com>
2025-04-10 19:52:12 +0200

    Changed AGHLeaveIndicator message

--- components/map/AGHLeaveIndicator.tsx
+++ components/map/AGHLeaveIndicator.tsx
@@ -4,7 +4,7 @@ function AGHLeaveIndicator() {
   return (
     <div className="pointer-events-none absolute left-0 top-0 box-border size-full border-4 border-errorAlert-foreground">
       <div className="absolute bottom-3 left-1/2 w-max max-w-[80%] -translate-x-1/2 rounded-md bg-errorAlert-foreground p-3 text-center">
-        Znajdujesz się poza obszarem dni otwartych AGH
+        Znajdujesz się poza obszarem Dni Otwartych AGH na WEAIiIB
       </div>
     </div>
   );
